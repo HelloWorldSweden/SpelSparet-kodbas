19c326d11288320a785ca633d53893aed8ca518a
kalle (jag) <kalle.jillheden@me.com>
2017-07-07 00:04:22 +0200

    LEvel selection

---
+++ Assets/SceneLoading.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Som exempel objekt för att ladda en annan scen.
+ * Samt kan Unity UI buttons länkas till LoadScene funktionen
+ */
+public class SceneLoading : MonoBehaviour {
+
+	public void LoadScene(string name) {
+		SceneManager.LoadScene(name);
+	}
+
+}
